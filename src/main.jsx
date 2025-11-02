fdbd434ab2e87a581895806ce609ae70658cae01
Tiagliveira <tiagliveira92@gmail.com>
2025-11-02 03:51:58 -0300

    foi criando uma aquilos de layout com react Outlet para padronizar as telas que iram apresenta o footer e o header separando ads demais

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { Elements } from '@stripe/react-stripe-js';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import { ThemeProvider } from 'styled-components';
 import stripePromisse from './config/stripeConfig';
 import AppProvider from './hooks';
-import { router } from './routes';
+import { Router } from './routes';
 import GlobalStyles from './styles/globalStyles';
 import { standardTheme } from './styles/themes/standard'
 
@@ -15,7 +15,9 @@ createRoot(document.getElementById('root')).render(
 		<ThemeProvider theme={standardTheme}>
 			<AppProvider>
 				<Elements stripe={stripePromisse}>
-					<RouterProvider router={router} />
+					<BrowserRouter>
+						<Router />
+					</BrowserRouter>
 				</Elements>
 				<GlobalStyles />
 				<ToastContainer autoClose={2000} theme="dark" />
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,61 +1,39 @@
-import { createBrowserRouter } from 'react-router-dom';
-import { Footer, Header } from '../components';
+import { Route, Routes } from 'react-router-dom';
+
 import { Cart, Checkout, CompletePayment, Home, Login, Menu, Register } from '../containers';
+import { UserLayout } from '../layouts/UserLayout';
+
+
+
+export function Router() {
+	return (
+		<Routes>
+			<Route path="/" element={<UserLayout />}>
+				<Route path='/' element={<Home />} />
+				<Route path='/cardapio' element={<Menu />} />
+				<Route path='/carrinho' element={<Cart />} />
+				<Route path='/checkout' element={<Checkout />} />
+				<Route path='/complete' element={<CompletePayment />} />
+			</Route>
+
+			<Route path='/login' element={<Login />} />
+			<Route path='/users' element={<Register />} />
+		</Routes >
+	)
+}
 
 
 
 
-export const router = createBrowserRouter([
-	{
-		path: '/login',
-		element: <Login />,
-	},
-	{
-		path: '/',
-		element: (
-			<>
-				<Header />
-				<Home />
-				<Footer />
-			</>
-		),
-	},
-	{
-		path: '/users',
-		element: <Register />,
-	},
-	{
-		path: '/cardapio',
-		element: (
-			<>
-				<Header />
-				<Menu />
-				<Footer />
-			</>
-		),
-	},
-	{
-		path: '/carrinho',
-		element: (
-			<>
-				<Cart />
-			</>
-		)
-	},
-	{
-		path: '/checkout',
-		element: (
-			<>
-				<Checkout />
-			</>
-		)
-	},
-	{
-		path: '/complete',
-		element: (
-			<>
-				<CompletePayment />
-			</>
-		)
-	}
-]);
+
+
+
+// 	{
+// 		path: '/complete',
+// 		element: (
+// 			<>
+// 				<CompletePayment />
+// 			</>
+// 		)
+// 	}
+// ]);
